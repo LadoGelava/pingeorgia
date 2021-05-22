a736d5eed7cc5ed16824ba30791bdabe474f6608
LadoGelava <56440841+LadoGelava@users.noreply.github.com>
2021-05-22 04:55:29 -0700

    Add files via upload

---
+++ tours.js
@@ -0,0 +1,23 @@
+const tours = [
+    {
+        id: 1,
+        category: 'Day Trip',
+        title: 'Mtirala National Park',
+        img: '',
+        description: 'This subtropical tour is for nature lovers with ...'
+    },
+    {
+        id: 2,
+        category: 'Day Trip',
+        title: 'Svaneti Hiking Tour',
+        description: 'The trip to highland region of Georgia, that yo ...',
+        img: ''
+    },
+    {
+        id: 3,
+        category: 'Day Trip',
+        title: 'Fortress, Bridge & Waterfall',
+        description: 'This is a less time tour without lunch. The tou ...',
+        img: ''
+    }
+]
